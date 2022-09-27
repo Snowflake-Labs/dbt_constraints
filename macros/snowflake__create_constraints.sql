6dc480570b085525481d2b4cf70e2643bbbadd50
Dan Flippo <dan.flippo@snowflake.com>
2022-09-26 21:53:22 -0400

    Snowflake: skip existing not null constraints

--- macros/snowflake__create_constraints.sql
+++ macros/snowflake__create_constraints.sql
@@ -92,16 +92,20 @@
 
     {%- if dbt_constraints.have_ownership_priv(table_relation, verify_permissions) -%}
 
-            {%- set modify_statements= [] -%}
-            {%- for column in columns_list -%}
+        {%- set modify_statements= [] -%}
+        {%- for column in columns_list -%}
+            {%- if not dbt_constraints.snowflake__not_null_exists(table_relation, column) -%}
                 {%- set modify_statements = modify_statements.append( "COLUMN " ~ column ~ " SET NOT NULL" ) -%}
-            {%- endfor -%}
+            {%- endif -%}
+        {%- endfor -%}
+        {%- if modify_statements | count > 0 -%}
             {%- set modify_statement_csv = modify_statements | join(", ") -%}
             {%- set query -%}
                 ALTER TABLE {{table_relation}} MODIFY {{ modify_statement_csv }};
             {%- endset -%}
             {%- do log("Creating not null constraint for: " ~ columns_list | join(", ") ~ " in " ~ table_relation, info=true) -%}
             {%- do run_query(query) -%}
+        {%- endif -%}
 
     {%- else -%}
         {%- do log("Skipping not null constraint for " ~ columns_list | join(", ") ~ " in " ~ table_relation ~ " because of insufficient privileges: " ~ table_relation, info=true) -%}
@@ -164,6 +168,25 @@
 
     {#- If we get this far then the table does not have this constraint -#}
     {%- do log("No FK key: " ~ table_relation ~ " " ~ column_names, info=false) -%}
+    {{ return(false) }}
+{%- endmacro -%}
+
+
+
+{#- This macro is used in create macros to avoid duplicate not null constraints -#}
+{%- macro snowflake__not_null_exists(table_relation, column_name) -%}
+    {%- set lookup_query -%}
+    SHOW COLUMNS LIKE '{{column_name}}' IN TABLE {{table_relation}}
+    {%- endset -%}
+    {%- do log("Lookup: " ~ lookup_query, info=false) -%}
+    {%- set column_list = run_query(lookup_query) -%}
+    {%- if column_list and 'NOT_NULL' in column_list.columns["null?"].values() -%}
+        {%- do log("Found not null constraint: " ~ table_relation ~ " " ~ column_name, info=false) -%}
+        {{ return(true) }}
+    {%- endif -%}
+
+    {#- If we get this far then the table does not have this constraint -#}
+    {%- do log("No not null constraint: " ~ table_relation ~ " " ~ column_name, info=false) -%}
     {{ return(false) }}
 {%- endmacro -%}
 
